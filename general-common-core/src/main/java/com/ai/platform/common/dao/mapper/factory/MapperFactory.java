4c432acf5b251710a62566c4d45d9b7c8583dc6d
gucl <gucl@asiainfo.com>
2016-08-30 12:46:58 +0800

    合并原slp-common到general-common

--- general-common-core/src/main/java/com/ai/platform/common/dao/mapper/factory/MapperFactory.java
+++ general-common-core/src/main/java/com/ai/platform/common/dao/mapper/factory/MapperFactory.java
@@ -7,6 +7,15 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.ai.platform.common.dao.mapper.interfaces.GnAreaMapper;
+import com.ai.platform.common.dao.mapper.interfaces.GnIndustryMapper;
+import com.ai.platform.common.dao.mapper.interfaces.GnIpAddrMapper;
+import com.ai.platform.common.dao.mapper.interfaces.GnServiceNumMapper;
+import com.ai.platform.common.dao.mapper.interfaces.GnSettleRuleMapper;
+import com.ai.platform.common.dao.mapper.interfaces.GnSubjectFundMapper;
+import com.ai.platform.common.dao.mapper.interfaces.GnSubjectMapper;
+import com.ai.platform.common.dao.mapper.interfaces.GnSysParamMapper;
+import com.ai.platform.common.dao.mapper.interfaces.GnTenantMapper;
 import com.ai.platform.common.dao.mapper.interfaces.SysOfficeMapper;
 import com.ai.platform.common.dao.mapper.interfaces.SysUserMapper;
 import com.ai.platform.common.dao.mapper.interfaces.SysWaitjobsMapper;
@@ -43,4 +52,42 @@ public class MapperFactory {
     public static SysWaitjobsMapper getSysWaitjobsMapper(){
     	return sqlSessionTemplate.getMapper(SysWaitjobsMapper.class);
     }
+    
+    public static GnSysParamMapper getGnSysParamMapper() {
+        return sqlSessionTemplate.getMapper(GnSysParamMapper.class);
+    }
+
+    public static GnAreaMapper getGnAreaMapper() {
+        return sqlSessionTemplate.getMapper(GnAreaMapper.class);
+    }
+
+    public static GnSubjectMapper getGnSubjectMapper() {
+        return sqlSessionTemplate.getMapper(GnSubjectMapper.class);
+    }
+
+    public static GnSubjectFundMapper getGnSubjectFundMapper() {
+        return sqlSessionTemplate.getMapper(GnSubjectFundMapper.class);
+    }
+
+    public static GnSettleRuleMapper getGnSettleRuleMapper() {
+        return sqlSessionTemplate.getMapper(GnSettleRuleMapper.class);
+    }
+    
+    public static GnTenantMapper getGnTenantMapper() {
+        return sqlSessionTemplate.getMapper(GnTenantMapper.class);
+    }
+    public static GnIpAddrMapper getGnIpAddrMapper() {
+    	return sqlSessionTemplate.getMapper(GnIpAddrMapper.class);
+    }
+    
+    public static GnServiceNumMapper getGnServiceNumMapper() {
+    	return sqlSessionTemplate.getMapper(GnServiceNumMapper.class);
+    }
+
+    public static GnIndustryMapper getGnIndustryMapper() {
+        return sqlSessionTemplate.getMapper(GnIndustryMapper.class);
+    }
+    
+    
+    
 }
