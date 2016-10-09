4bb8eaeef9452bbddffb73878b45b6524617e274
gucl <gucl@asiainfo.com>
2016-10-09 18:54:25 +0800

    v1.0_1

--- onekey-docker.sh
+++ onekey-docker.sh
@@ -1,11 +1,12 @@
+tagversion=v1.0_1
 git reset --hard origin/master 
 git pull 
 chmod a+x onekey-docker.sh 
 gradle clean && gradle build -x test 
-docker build -t 10.19.13.18:5000/general-common:v1.0_1 .   
-docker push 10.19.13.18:5000/general-common:v1.0_1 
+docker build -t 10.19.13.18:5000/general-common:${tagversion} .   
+docker push 10.19.13.18:5000/general-common:${tagversion} 
 
-docker rmi aioptapp/general-common:v1.0_1 
-docker tag 10.19.13.18:5000/general-common:v1.0_1 aioptapp/general-common:v1.0_1 
+docker rmi aioptapp/general-common:${tagversion} 
+docker tag 10.19.13.18:5000/general-common:${tagversion} aioptapp/general-common:${tagversion} 
 docker login --username=aioptapp --password=aioptapp@123 --email=wuzhen3@asiainfo.com 
-docker push aioptapp/general-common:v1.0_1 
+docker push aioptapp/general-common:${tagversion} 
